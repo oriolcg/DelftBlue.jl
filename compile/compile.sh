d6f585c586c02560368e91e8c3ca16e08b1023a0
Oriol Colomes Gene <ocolomesgene@login03.delftblue.tudelft.net>
2022-04-04 20:41:58 +0200

    petsc installation

--- compile/compile.sh
+++ compile/compile.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+module use /mnt/shared/apps/modulefiles
+module load 2022r1
+module load compute
+module load julia
+module load openmpi/4.1.1-urzuzcv
+module load petsc
+
 # This script is to be executed from this folder (compile/)
 # See https://juliaparallel.github.io/MPI.jl/latest/knownissues/#Julia-module-precompilation-1
 # for a justification of this line
---
+++ compile/install_petsc.sh
@@ -0,0 +1,28 @@
+module use /mnt/shared/apps/modulefiles
+module load 2022r1
+module load compute
+module load cmake
+module load openmpi
+module load blas/gcc/64/3.8.0
+module load lapack/gcc/64/3.9.0
+
+CURR_DIR=$(pwd)
+PACKAGE=petsc
+VERSION=3.15.4
+INSTALL_ROOT=$HOME/progs/install
+PETSC_INSTALL=$INSTALL_ROOT/$PACKAGE/$VERSION
+TAR_FILE=$PACKAGE-$VERSION.tar.gz
+URL="https://ftp.mcs.anl.gov/pub/petsc/release-snapshots/"
+ROOT_DIR=$HOME/progs
+SOURCES_DIR=$ROOT_DIR/$PACKAGE-$VERSION
+BUILD_DIR=$SOURCES_DIR/build
+wget -q $URL/$TAR_FILE -O $ROOT_DIR/$TAR_FILE
+mkdir -p $SOURCES_DIR
+tar xzf $ROOT_DIR/$TAR_FILE -C $SOURCES_DIR --strip-components=1
+cd $SOURCES_DIR
+./configure --prefix=$PETSC_INSTALL --with-cc=mpicc --with-cxx=mpicxx --with-fc=mpif90 \
+            --download-mumps --download-scalapack --download-parmetis --download-metis \
+            --download-ptscotch --with-debugging --with-x=0 --with-shared=1 \
+            --with-mpi=1 --with-64-bit-indices --with-shared-libraries=0
+make
+make install
